49220988e53b94de35b741a007a1bd958993cff9
Sara L Riley <saralynnaz@gmail.com>
2017-03-28 14:20:49 +0100

    Add interactive menu

--- directory.rb
+++ directory.rb
@@ -1,7 +1,36 @@
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      #input the students
+      students = input_students
+    when "2"
+      #show the students
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit #this will cause the program to terminate
+    else
+      puts "I don't kow what you meant, try again"
+    end
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #create and empty array
+  #create an empty array
   students = []
   #get the first name
   name = gets.chomp
@@ -32,12 +61,15 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+interactive_menu
+
+=begin TO DELETE
 students = input_students
 #nothing happens until we call the methods
 print_header
 print(students)
 print_footer(students)
-
+=end
 
 =begin
 #create nested array with both name and cohort month
