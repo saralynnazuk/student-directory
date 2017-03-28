7bfcbcc5131803333bbce25d07b32cd210e2da07
Sara L Riley <saralynnaz@gmail.com>
2017-03-28 15:31:14 +0100

    Refactor code

--- directory.rb
+++ directory.rb
@@ -1,49 +1,49 @@
-def interactive_menu
-  students = []
-  loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      #input the students
-      students = input_students
-    when "2"
-      #show the students
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit #this will cause the program to terminate
-    else
-      puts "I don't kow what you meant, try again"
-    end
-  end
-end
+@students = [] #an empty array accessibel to all methods
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #create an empty array
-  students = []
   #get the first name
   name = gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
-    #add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     #get another name from the user
     name = gets.chomp
   end
-  #return the array of students
-  students
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit #this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
 end
 
 def print_header
@@ -51,39 +51,14 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student [:cohort]} cohort)"
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 interactive_menu
-
-=begin TO DELETE
-students = input_students
-#nothing happens until we call the methods
-print_header
-print(students)
-print_footer(students)
-=end
-
-=begin
-#create nested array with both name and cohort month
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name:"Darth Vader", cohort: :november},
-  {name:"Nurse Ratched", cohort: :november},
-  {name:"Michael Corleone", cohort: :november},
-  {name:"Alex DeLarge", cohort: :november},
-  {name:"The Wicked Witch of the West", cohort: :november},
-  {name:"Terminator", cohort: :november},
-  {name:"Freddy Krueger", cohort: :november},
-  {name:"The Joker", cohort: :november},
-  {name:"Joffrey Baratheon", cohort: :november},
-  {name:"Norman Bates", cohort: :november}
-]
-=end
